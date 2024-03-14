0dd550b54b654f2f3ba4df847f0debef271ed7b4
dungggg1203 <dungcongnghiep4@gmail.com>
2024-03-15 00:25:21 +0700

    15032024

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,4 +1,4 @@
-import React, {Component, useState} from 'react';
+import React, {Component, useEffect, useState} from 'react';
 import {View, Text, TouchableOpacity, ImageBackground, Dimensions, TextInput, Image} from 'react-native';
 // eslint-disable-next-line no-undef
 windowWidth = Dimensions.get('window').width;
@@ -8,6 +8,30 @@ windowHeight = Dimensions.get('window').height;
 const Home = ({route, navigation}) => {
   const {params} = route;
   const email = params && params.email ? params.email : '';
+
+  const [number, setNumber] = useState(0);
+  const [check, setCheck] = useState(false);
+  const [checkPause, setCheckPause] = useState(false);
+  const [checkReset, setCheckReset] = useState(false);
+
+  useEffect(() => {
+    if (check) {
+      const interval = setInterval(() => {
+        if (number < 100) {
+          setNumber((prevNumber) => prevNumber + 1);
+        }
+      }, 1000); // Mỗi giây
+      return () => clearInterval(interval);
+    }
+  }, [number, check, checkPause]);
+
+  const resetNumber = useEffect(() => {
+    if (checkReset == true) {
+      setNumber(0);
+    }
+    setCheckReset(false);
+  });
+
   return (
     <View>
       <TouchableOpacity
@@ -26,11 +50,75 @@ const Home = ({route, navigation}) => {
         <Text style={{fontWeight: 700}}>Chào :{email ? email : 'Khách'} </Text>
       </View>
       <View style={{justifyContent: 'center', alignItems: 'center', marginTop: '10%'}}>
-        <Text style={{fontWeight: 700, fontSize: 30}}>1</Text>
+        <Text style={{fontWeight: 700, fontSize: 30}}>{number}</Text>
       </View>
       <View style={{justifyContent: 'center', alignItems: 'center', marginTop: '10%'}}>
-        <TouchableOpacity>
-          <Text style={{fontWeight: 700, fontSize: 15, borderWidth: 1, padding: 20, borderRadius: 20}}>Đếm số</Text>
+        <TouchableOpacity
+          onPress={() => {
+            if (!check) {
+              setCheck(true);
+            }
+          }}>
+          <Text
+            style={{
+              fontWeight: 700,
+              fontSize: 15,
+              paddingLeft: 34,
+              paddingRight: 34,
+              borderRadius: 20,
+              paddingTop: 15,
+              paddingBottom: 15,
+              backgroundColor: 'green',
+            }}>
+            Đếm số
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={{justifyContent: 'center', alignItems: 'center', marginTop: '10%'}}>
+        <TouchableOpacity
+          onPress={() => {
+            if (checkPause == false) {
+              setCheckPause(true);
+            }
+          }}>
+          <Text
+            style={{
+              fontWeight: 700,
+              fontSize: 15,
+              paddingLeft: 28,
+              paddingRight: 28,
+              borderRadius: 20,
+              paddingTop: 15,
+              paddingBottom: 15,
+              backgroundColor: 'green',
+            }}>
+            Dừng đếm
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={{justifyContent: 'center', alignItems: 'center', marginTop: '10%'}}>
+        <TouchableOpacity
+          onPress={() => {
+            if (checkReset == false) {
+              setCheckReset(true);
+              resetNumber;
+            }
+          }}>
+          <Text
+            style={{
+              fontWeight: 700,
+              fontSize: 15,
+              paddingLeft: 28,
+              paddingRight: 28,
+              borderRadius: 20,
+              paddingTop: 15,
+              paddingBottom: 15,
+              backgroundColor: 'green',
+            }}>
+            Reset đếm
+          </Text>
         </TouchableOpacity>
       </View>
     </View>
